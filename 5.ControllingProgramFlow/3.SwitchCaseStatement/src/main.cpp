257518d5d9a1c78b039a641e574dec4affa631e7
Kamoliddin Soliev <69765410+KamoliddinS@users.noreply.github.com>
2023-10-08 17:33:35 +0500

    updates

--- 5.ControllingProgramFlow/3.SwitchCaseStatement/src/main.cpp
+++ 5.ControllingProgramFlow/3.SwitchCaseStatement/src/main.cpp
@@ -13,8 +13,17 @@ int main() {
             std::cout << "Sunday" << std::endl;
             break;
         case 2:
-            std::cout << "Monday" << std::endl;
-            break;
+            {
+                char choice {}; // if we don't use {} here, the variable choice will be available outside the switch statement
+                std::cout << "Are you sure you want to enter 2? (y/n): ";
+                std::cin >> choice;
+                if (choice == 'y' || choice == 'Y') {
+                    std::cout << "Monday" << std::endl;
+                } else {
+                    std::cout << "You entered 2 but you don't want to see Monday." << std::endl;
+                }
+                break;
+            }
         case 3:
             std::cout << "Tuesday" << std::endl;
             break;
@@ -30,6 +39,7 @@ int main() {
         case 7:
             std::cout << "Saturday" << std::endl;
             break;
+            
         default:
             std::cout << "Invalid input. Please enter a number from 1 to 7." << std::endl;
     }
